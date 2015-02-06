353671396b7a8202983a5200f31e9ae488c9a779
Adam Knox <aknox@zulily.com>
2015-02-05 17:53:21 -0800

    add the question list module, and we are printing out the response

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var stackApi = (function(){
 	var baseUrl = "https://api.stackexchange.com/2.2/search?key="+key+"&site=stackoverflow&order=desc&sort=activity&filter=default";
 
 	function _search(query, callback) {
-		console.log(query);
 		var url = baseUrl + '&tagged=' + query;
 		$.get(url).success(callback);
 	}
@@ -14,13 +13,36 @@ var stackApi = (function(){
 	};
 })();
 
+var questionList = (function(){
+	var $questionList = $('.js-questions')
+
+	function _refresh(queryData) {
+		var questionListHtml = '';
+		$.each(queryData.items, function(i,item){
+			questionListHtml += '<li>';
+			questionListHtml += item.title
+			questionListHtml += '</li>';
+		});
+
+		$questionList.html(questionListHtml);
+	}
+
+	return {
+		refresh: _refresh
+	}
+})();
+
 // Fire it up
 $(function(){
 	$('.js-search-form').submit(function(e){
 		e.preventDefault();
 
 		var query = $(this).find('[name=query]').val();
-		var questions = stackApi.search(query);
+		stackApi.search(query,function(queryResponse){
+			questionList.refresh(queryResponse);
+		});
 
-	})
+
+		
+	});
 });
